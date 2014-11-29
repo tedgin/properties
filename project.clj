c13d79bae7e92c052b0c99593f40f1b1a7dd9ecc
Tony Edgin <tedgin@iplantcollaborative.org>
2014-11-29 12:38:58 -0700

    added project URL

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description  "A clojure library that unifies property determination into a single interface that 
                  supports, but does not require, type checking and validation."
   :license      {:name "Public Domain"}
+  :url          "https://github.com/tedgin/properties"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "1.0.0"]]
   :plugins      [[lein-environ "1.0.0"]]
